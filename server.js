4eaa499c2b1837e80fee67090542f2581b595db1
michaelzhang114 <michaelzhang6284@gmail.com>
2024-01-08 18:55:05 -0800

    added some styling

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express"); //Import the express dependency
 const app = express(); //Instantiate an express app, the main work horse of this server
 const port = 5050; //Save the port number where your server will be listening
 
+app.use(express.static("public"));
+
 //Idiomatic expression in express to route and respond to a client request
 app.get("/", (req, res) => {
 	//get requests to the root ("/") will route here
