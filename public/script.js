42a61f7d05bf997be6edb7f3eb852c9a0940aeed
michaelzhang114 <michaelzhang6284@gmail.com>
2024-01-14 00:21:25 -0800

    created modals

--- public/script.js
+++ public/script.js
@@ -727,8 +727,39 @@ async function initAllPlayerData() {
 	}
 }
 
+function initHelpButton() {
+	const openModal = document.getElementById("help-modal-open");
+	const myHelpModal = document.getElementById("help-modal");
+	const closeModal = document.getElementById("close-help");
+
+	openModal.addEventListener("click", () => {
+		myHelpModal.showModal();
+	});
+
+	closeModal.addEventListener("click", () => {
+		myHelpModal.close();
+	});
+}
+
+function initStatsButton() {
+	const openModal = document.getElementById("stats-modal-open");
+	const myStatsModal = document.getElementById("stats-modal");
+	const closeModal = document.getElementById("close-stats");
+
+	openModal.addEventListener("click", () => {
+		myStatsModal.showModal();
+	});
+
+	closeModal.addEventListener("click", () => {
+		myStatsModal.close();
+	});
+}
+
 async function main() {
 	try {
+		initHelpButton();
+		initStatsButton();
+
 		fullPlayerData = await initAllPlayerData();
 
 		initAnswer().then(() => {
