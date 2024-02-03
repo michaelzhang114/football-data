9fb582257ad37eabc2b887d4a9214be4b27ecba1
michaelzhang114 <michaelzhang6284@gmail.com>
2024-02-03 09:35:18 -0800

    copy butn revert back to click event

--- public/helpers.js
+++ public/helpers.js
@@ -67,8 +67,8 @@ export function showCopyText(tmp) {
 function initCopyButton() {
 	const btn = document.getElementById("copy-button");
 	// btn.addEventListener("touchstart", handleCopy);
-	// btn.addEventListener("click", handleCopy);
-	btn.addEventListener("pointerdown", handleCopy);
+	btn.addEventListener("click", handleCopy);
+	// btn.addEventListener("pointerdown", handleCopy);
 }
 
 function handleCopy(evt) {
@@ -107,9 +107,9 @@ function handleCopy(evt) {
 		copyButton.classList.remove("copied");
 	}, 2000);
 
-	if (evt.pointerType === "touch") {
-		evt.preventDefault();
-	}
+	// if (evt.pointerType === "touch") {
+	// 	evt.preventDefault();
+	// }
 }
 
 export function isNameInArray(array, targetName) {
